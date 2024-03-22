0addf03accb5f24f63fb46f88571fdc88e23cbc0
ivan-rozgon <ivan.rozgon08@mail.ru>
2024-03-22 18:52:06 +0300

    добавляем капитана

--- main.py
+++ main.py
@@ -1,72 +1,57 @@
-from flask import Flask, render_template
+from flask import Flask
+from data import db_session
+from data.users import User
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-@app.route('/promotion')
-def promotion():
-    promo = ["Человечество вырастает из детства.", "Человечеству мала одна планета.",
-             "Мы сделаем обитаемыми безжизненные пока планеты.", "И начнем с Марса!", "Присоединяйся!"]
-    return '</br>'.join(promo)
+db_session.global_init("db/mars_explorer.db")
+db_sess = db_session.create_session()
+
+user = User()
+user.surname = "Scott"
+user.name = "Ridley"
+user.age = 21
+user.position = "captain"
+user.speciality = "research engineer"
+user.adress = "module_1"
+user.email = "scott_chief@mars.org"
+
+user = User()
+user.surname = "Harris"
+user.name = "Leo"
+user.age = 25
+user.position = "worker"
+user.speciality = "engineer"
+user.adress = "module_2"
+user.email = "leo_harris@mars.org"
+
+user = User()
+user.surname = "Carter"
+user.name = "Anthony"
+user.age = 28
+user.position = "worker"
+user.speciality = "developer"
+user.adress = "module_1"
+user.email = "antony_carter@mars.org"
+
+user = User()
+user.surname = "Cobb"
+user.name = "Steven"
+user.age = 30
+user.position = "worker"
+user.speciality = "cook"
+user.adress = "module_3"
+user.email = "steven_cobb@mars.org"
+
+db_sess.add(user)
+db_sess.commit()
 
 
-@app.route('/image_mars')
-def img():
-    return """<!doctype html>
-                <html>
-                  <head>
-                    <meta charset="UTF-8">
-                      <title>Привет, Марс!</title>
-                  </head>
-                  <body>
-                    <h1>Жди нас, Марс!</h1>
-                    <figure>
-                        <img src="static/img.png">
-                        <figcaption>
-                        Вот она какая, красная планета
-                        </figcaption>
-                    </figure>
-                  </body>
-                </html>"""
-
-
-@app.route('/choice/<planet_name>')
-def planet(planet_name):
-    return f'''<!doctype html>
-                <html>
-                  <head>
-                    <meta charset="UTF-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                      <title>Варианты выбора</title>
-                      <link rel="stylesheet"
-                                   href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-                                   integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-                                   crossorigin="anonymous">
-                  </head>
-                  <body>
-                    <h1>Мое предложение: {planet_name}</h1>
-                    <h2>Эта планета ближже к Земле;<h2>
-                    <div class="alert alert-success" role="alert">
-                      На ней много необходимых ресурсов;
-                    </div>
-                    <div class="alert alert-secondary" role="alert">
-                      На ней есть вода и атмосфера;
-                    </div>
-                    <div class="alert alert-warning" role="alert">
-                      На ней есть небольшое магнитное поле;
-                    </div>
-                    <div class="alert alert-danger" role="alert">
-                      Наконец, она просто красива!
-                    </div>
-                  </body>
-                </html>'''
-
-
-@app.route('/')
-@app.route('/index/<title>')
-def page(title):
-    return render_template('base.html', title=title)
+def main():
+    app.run()
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    main()
