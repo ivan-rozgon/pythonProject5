95dc8dfad541bb775b18b120a677983da36eb2ff
ivan-rozgon <ivan.rozgon08@mail.ru>
2024-03-22 19:10:18 +0300

    добавляем капитана (решение)

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import datetime
+
 from flask import Flask
 from data import db_session
+from data.jobs import Jobs
 from data.users import User
 
 app = Flask(__name__)
@@ -56,9 +59,13 @@ user.email = "steven_cobb@mars.org"
 db_sess.add(user)
 db_sess.commit()
 
-
-db_sess.add(user)
-db_sess.commit()
+job = Jobs()
+job.team_leader = 1
+job.job = "deployment of residential modules 1 and 2"
+job.work_size = 15
+job.collaborators = "2, 3"
+job.start_date = datetime.datetime.now()
+job.is_finished = False
 
 
 def main():
